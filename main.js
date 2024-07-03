b3b13fd5d76f056841ac387c5eced347d1e1543a
kvtrice <katricemountford@gmail.com>
2024-07-03 16:15:20 +1000

    Adds js functions to translate

---
+++ main.js
@@ -0,0 +1,21 @@
+import {
+	morseToEnglishLowerCase,
+	translateEnglishToMorse,
+	translateMorseToEnglish,
+} from "./modules/translator.js";
+
+const englishTextBox = document.getElementById("english");
+const morseTextBox = document.getElementById("morse");
+
+englishTextBox.addEventListener("input", e => {
+	const englishText = e.target.value;
+	const morseText = translateEnglishToMorse(englishText);
+	morseTextBox.value = morseText;
+});
+
+morseTextBox.addEventListener("input", e => {
+	const morseText = e.target.value;
+	const capitalisedEnglishText = translateMorseToEnglish(morseText);
+	const cleanedEnglishText = morseToEnglishLowerCase(capitalisedEnglishText);
+	englishTextBox.value = cleanedEnglishText;
+});
---
+++ modules/translator.js
@@ -0,0 +1,44 @@
+import { morseChars } from "./data.js";
+
+export const translateEnglishToMorse = englishInput => {
+	const toCapitals = englishInput.toUpperCase();
+	const textArray = toCapitals.split("");
+	return textArray
+		.map(char => {
+			if (char === " ") {
+				return "   ";
+			}
+
+			return morseChars[char] || "";
+		})
+		.join(" ");
+};
+
+export const translateMorseToEnglish = morseInput => {
+	const words = morseInput.split("   ");
+	console.log(words);
+	const characters = words.map(word =>
+		word
+			.split(" ")
+			.map(morseChar =>
+				Object.keys(morseChars).find(
+					key => morseChars[key] === morseChar
+				)
+			)
+			.join("")
+	);
+	return characters.join(" ");
+};
+
+export const morseToEnglishLowerCase = capitalString => {
+	const wordsArr = capitalString.split(" ");
+	return wordsArr
+		.map((word, i) => {
+			if (i === 0) {
+				return word.charAt(0) + word.slice(1).toLowerCase();
+			} else {
+				return word.toLowerCase();
+			}
+		})
+		.join(" ");
+};
